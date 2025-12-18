12009e7847a6d27888c6b807c495fff987af3e64
Danilo-Aguiar <danilo2021.aguiar@gmail.com>
2025-12-18 18:36:51 -0300

    Update xadrez.c
    Script feito no VS Code e colado aqui.

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,48 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+// Torres
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+int main(){
+    int movimentoTorre = 5;
+    int i = 1;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("Torre\n");
+    printf("\n");
+    for(i; i <= movimentoTorre; i++){
+       printf("Direita\n");
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    // Bispo
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    int movimentoBispo = 5;
+    int ib = 1;
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
+    printf("Bispo\n");
+    printf("\n");
+    while(movimentoBispo >= ib){
+        printf("Cima, Direita\n");
+        ib++;
+    }
+
+    printf("\n");
+
+    int movimentoRainha = 8;
+    int ir = 1;
+
+    // Rainha
+
+    printf("Rainha\n");
+    printf("\n");
+
+    do{
+        printf("Esquerda\n");
+        ir++;
+    }while(movimentoRainha >= ir);
+
+    printf("\n");
+
 }
