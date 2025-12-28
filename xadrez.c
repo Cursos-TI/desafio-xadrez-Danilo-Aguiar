0effcc05f253ac5ef1aa867285f90bcc7104e4c5
Danilo-Aguiar <danilo2021.aguiar@gmail.com>
2025-12-28 12:22:42 -0300

    Update xadrez.c
    Foi adicionado o movimento do cavalo com loops aninhados.

--- xadrez.c
+++ xadrez.c
@@ -45,4 +45,29 @@ int main(){
 
     printf("\n");
 
+    // Cavalo
+
+    printf("Cavalo\n");
+    printf("\n");
+    int casasBaixo = 2;
+    int casasEsquerda = 1;
+
+    for (int etapa = 1; etapa <= 2; etapa++) {
+
+        /* Primeira etapa: duas casas para baixo */
+        if (etapa == 1) {
+            for (int i = 1; i <= casasBaixo; i++) {
+                printf("Baixo\n");
+            }
+        }
+
+        /* Segunda etapa: uma casa para a esquerda */
+        if (etapa == 2) {
+            for (int i = 1; i <= casasEsquerda; i++) {
+                printf("Esquerda\n");
+            }
+        }
+    }
+
+    return 0;
 }
